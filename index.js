bbaa8afa587b69b749b2e878fcd816e26ad18606
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-10-29 04:39:14 +0600

    post request method added

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
 app.get("/", (req, res) => {
   res.send("Simple Node Server is running");
 });
+
+app.use(cors());
+app.use(express.json());
 
 const users = [
   { id: 1, name: "John Doe" , email:'johndoe@gmail.com' },
@@ -15,6 +19,14 @@ const users = [
 
 app.get('/users', (req, res) => {
   res.send(users);
+})
+
+app.post('/users',(req,res)=>{
+  console.log('post request received');
+  const user = req.body;
+  user.id = users.length + 1;
+  users.push(user);
+  res.send(user);
 })
 
 
