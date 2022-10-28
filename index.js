8a9255341a0eacb2088bcf1929bf2a7c58d6ad7a
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-10-29 03:41:40 +0600

    users added

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ app.get("/", (req, res) => {
   res.send("Simple Node Server is running");
 });
 
+const users = [
+  { id: 1, name: "John Doe" , email:'johndoe@gmail.com' },
+  { id: 2, name: "Jane Doe", email:'janedoe@gmail.com' },
+  { id: 3, name: "John Smith", email:'johnsmith1@gmail.com' },
+  { id: 4, name: "Jane Smith" , email:'janesmith@gmail.com'},
+];
+
+app.get('/users', (req, res) => {
+  res.send(users);
+})
+
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
